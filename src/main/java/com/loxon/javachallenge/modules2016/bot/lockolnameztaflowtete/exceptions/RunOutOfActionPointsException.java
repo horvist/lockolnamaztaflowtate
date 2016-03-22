2fe834bb06fa686973c1e5f8e7c1ac0aea7fe60c
rkalmar <rolandkalmar@gmail.com>
2016-03-22 22:58:21 +0100

    Repair subclass.

--- src/main/java/com/loxon/javachallenge/modules2016/bot/lockolnameztaflowtete/exceptions/RunOutOfActionPointsException.java
+++ src/main/java/com/loxon/javachallenge/modules2016/bot/lockolnameztaflowtete/exceptions/RunOutOfActionPointsException.java
@@ -3,7 +3,7 @@ package com.loxon.javachallenge.modules2016.bot.lockolnameztaflowtete.exceptions
 /**
  * @author kalmarr
  */
-public class RunOutOfActionPointsException extends RunOutOfTimeException {
+public class RunOutOfActionPointsException extends Exception {
 
     public RunOutOfActionPointsException(String msg){
         super(msg);
